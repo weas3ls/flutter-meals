54096dbd2d925c3e0c8e540ee8749b7db170fe91
weas3ls <shabaneh.ahmad@gmail.com>
2024-06-14 15:48:04 -0700

    implement meals provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals/screens/tabs.dart';
@@ -14,7 +15,11 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
