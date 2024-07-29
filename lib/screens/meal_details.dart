0e20eda989cec038606b8251504a106ba6a08c3f
weas3ls <shabaneh.ahmad@gmail.com>
2024-07-29 12:52:26 -0700

    implement implicit animation for favorite icon in meals detail screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -37,7 +37,17 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) => RotationTransition(
+                turns: Tween<double>(begin: 0.5, end: 1).animate(animation),
+                child: child,
+              ),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
